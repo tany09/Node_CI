507ebcc792c64fec6e7eb115633af55af05f2361
Tanveer Singh Dhawan <tanysingh09@gmail.com>
2019-07-28 16:29:47 +0530

    Changed chrome option headless:true

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -4,7 +4,7 @@ const userFactory = require('../factories/userFactory');
 
 class CustomPage {
     static async build() {
-        const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox']});
+        const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
         const page = await browser.newPage();
         const customPage = new CustomPage(page);
         return new Proxy(customPage, {
